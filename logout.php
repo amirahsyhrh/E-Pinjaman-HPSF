3b29a353ecb13b0937fa4979101ea26d249921b1
Amirah <amirahsyahirah8800@gmail.com>
2023-06-26 14:51:14 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,7 +9,7 @@
 	<?php   
 session_start(); //to ensure you are using same session
 session_destroy(); //destroy the session
-header("location:/booking1/index.php"); //to redirect back to "index.php" after logging out
+header("location:/pinjaman/index.php"); //to redirect back to "index.php" after logging out
 exit();
 ?>
 </body>
